4620a1c15235006230ac6e9cfe0495e4b639958f
Julio Benavente <julio.benavente.02@gmail.com>
2023-03-11 23:42:04 -0500

    Task

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -5,18 +5,70 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import { useEffect } from "react";
 
+const defaultColDef = {
+  sortable: true,
+  // filter: true,
+};
+
 const columnDefs: ColDef[] = [
-  { field: "designation", headerName: "Designation" },
-  { field: "discovery_date", headerName: "Discovery Date" },
-  { field: "h_mag", headerName: "H (mag)" },
-  { field: "moid_au", headerName: "MOID (au)" },
-  { field: "q_au_1", headerName: "q (au)" },
-  { field: "q_au_2", headerName: "Q (au)" },
-  { field: "period_yr", headerName: "Period (yr)" },
-  { field: "i_deg", headerName: "Inclination (deg)" },
-  { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true },
+  { field: "designation", headerName: "Designation", filter: true },
+  {
+    field: "discovery_date",
+    headerName: "Discovery Date",
+    filter: false,
+  },
+  { field: "h_mag", headerName: "H (mag)", filter: "agNumberColumnFilter" },
+  { field: "moid_au", headerName: "MOID (au)", filter: "agNumberColumnFilter" },
+  { field: "q_au_1", headerName: "q (au)", filter: "agNumberColumnFilter" },
+  { field: "q_au_2", headerName: "Q (au)", filter: "agNumberColumnFilter" },
+  {
+    field: "period_yr",
+    headerName: "Period (yr)",
+    filter: "agNumberColumnFilter",
+  },
+  {
+    field: "i_deg",
+    headerName: "Inclination (deg)",
+    filter: "agNumberColumnFilter",
+  },
+  { field: "pha", headerName: "Potentially Hazardous", filter: true },
+  {
+    field: "orbit_class",
+    headerName: "Orbit Class",
+    enableRowGroup: true,
+    filter: true,
+  },
 ];
+
+interface IData {
+  designation: string;
+  discovery_date: Date;
+  h_mag: number;
+  moid_au: number;
+  q_au_1: number;
+  q_au_2: number;
+  period_yr: number;
+  i_deg: number;
+  pha: string;
+  orbit_class: string;
+}
+
+const formattingData = (data: any): IData[] => {
+  return data.map((e: IData) => ({
+    designation: e.designation,
+    discovery_date: new Date(e.discovery_date),
+    h_mag: Number(e.h_mag),
+    moid_au: Number(e.moid_au),
+    q_au_1: Number(e.q_au_1),
+    q_au_2: Number(e.q_au_2),
+    period_yr: Number(e.period_yr),
+    i_deg: Number(e.i_deg),
+    pha: e.pha,
+    orbit_class: e.orbit_class,
+  }));
+};
+
+const dataFormated = formattingData(data);
 
 const NeoGrid = (): JSX.Element => {
   useEffect(() => {
@@ -27,8 +79,9 @@ const NeoGrid = (): JSX.Element => {
     <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
       <h1>Near-Earth Object Overview</h1>
       <AgGridReact
-        rowData={data}
+        rowData={dataFormated}
         columnDefs={columnDefs}
+        defaultColDef={defaultColDef}
         rowGroupPanelShow={"always"}
       />
     </div>
